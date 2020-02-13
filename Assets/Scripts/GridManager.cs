8d165974eecf9997bb1b4e24c5a9b8b0af5f5cd5
HsiaoTe Tang <k50730@hotmail.com>
2020-02-13 14:05:07 -0500

    grid setup

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -14,11 +14,26 @@ public class GridManager : MonoBehaviour
     Cell[,] grid;
     public GameObject cellPrefab;
 
+    public int width;
+    public int height;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        cellPrefab = Resources.Load("Cell", typeof(GameObject)) as GameObject;   
+        cellPrefab = Resources.Load("Cell", typeof(GameObject)) as GameObject;
+
+        grid = new Cell[width, height];
+
+        for(int i = 0; i < width; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                grid[i, j] = Instantiate(cellPrefab, new Vector3(i, j, 0), cellPrefab.transform.rotation).GetComponent<Cell>();
+            }
+        }
+
+        Camera.main.transform.position = new Vector3(width / 2f, height / 2f, -50);
     }
 
     // Update is called once per frame
