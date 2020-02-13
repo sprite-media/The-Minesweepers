85899c9f93fa0f1cc9bd24d0ba162494d6127614
Jin <bomjan@naver.com>
2020-02-13 16:20:41 -0500

    Implemented player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,30 @@ using UnityEngine;
 //It will communicate with server and affect the game flow(player turn)
 public class GameManager : MonoBehaviour
 {
+	public static GameManager instance { get; private set; }
 	//will be used for other players
-	List<Player> players;
+	Dictionary<string, Player> players;
 	Player player;//own player
+
+	private void Awake()
+	{
+		if (instance == null)
+			instance = this;
+		else
+			Destroy(this);
+
+		player = new GameObject().AddComponent<Player>();
+		players = new Dictionary<string, Player>();
+		players.Add("testID", player);//player will always be at index 0
+		//TODO add network players
+	}
+
+	private void GivePermission(string id)
+	{
+		players[id].Turn();
+	}
+	public void NotifyTurnEnd()
+	{
+
+	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,8 @@
 
 public class Player : MonoBehaviour
 {
-	public bool turn { get; set; }
+	private bool turn;
+	private string networkID;
 	private Cell pressed;
 
 	private void Awake()
@@ -12,50 +13,55 @@ public class Player : MonoBehaviour
 
 	private void Update()
 	{
+		//Player can affect the grid only when it's his turn
 		if (!turn)
 			return;
+
 		RaycastHit hit;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+		//Ray hits something(there are cells only)
 		if (Physics.Raycast(ray, out hit, 1000))
 		{
 			Debug.DrawRay(ray.origin, ray.direction * 1000, Color.green);
-			if (Input.GetMouseButtonDown(0))
-			{
-				pressed = hit.collider.GetComponent<Cell>();
-			}
-			else if (Input.GetMouseButtonUp(0))
-			{
-				Cell hitCell = hit.collider.GetComponent<Cell>();
-				if (hitCell != null)
-				{
-					if (pressed == hitCell)
-					{
-						Debug.Log("Clicked");
-						//Call click function of pressed
-					}
-				}
-			}
-			else if (Input.GetMouseButtonDown(1))
-			{
-				pressed = hit.collider.GetComponent<Cell>();
-			}
-			else if (Input.GetMouseButtonUp(1))
-			{
-				Cell hitCell = hit.collider.GetComponent<Cell>();
-				if (hitCell != null)
-				{
-					if (pressed == hitCell)
-					{
-						Debug.Log("Flagged");
-						//Call flag function of pressed
-					}
-				}
-			}
+			SelectCell(0, hit);
+			SelectCell(1, hit);
 		}
 		else
 		{
 			Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
 		}
+	}
+
+	private void SelectCell(int mouseButton, RaycastHit hit)
+	{
+		if (Input.GetMouseButtonDown(mouseButton))
+		{
+			pressed = hit.collider.GetComponent<Cell>();
+			//Debug.Log(pressed);
+		}
+		else if (Input.GetMouseButtonUp(mouseButton))
+		{
+			Cell hitCell = hit.collider.GetComponent<Cell>();
+			//Debug.Log(hitCell);
+			if (hitCell != null)
+			{
+				if (pressed == hitCell)
+				{
+					//Call function based on mouseButton
+					FinishTurn();
+				}
+			}
+			pressed = null;
+		}
+	}
+	public void Turn()
+	{
+		turn = true;
+	}
+	private void FinishTurn()
+	{
+		turn = false;
+		GameManager.instance.NotifyTurnEnd();
 	}
 }
