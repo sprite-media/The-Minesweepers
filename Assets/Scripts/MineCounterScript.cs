82983906793e0cd1b8a1963adcf7de506918de9a
Ding Zhang <36141388+Dgit35265@users.noreply.github.com>
2020-02-14 02:53:29 -0500

    UI layout setup

---
+++ Assets/Scripts/MineCounterScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MineCounterScript : MonoBehaviour
+{
+    public Text MineCounterText;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MineCounterText.text = "0";   
+    }
+}
---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class TimerScript : MonoBehaviour
+{
+    public Text TimerText;
+
+    void Start()
+    {
+        //Get Time Reference
+    }
+    void Update()
+    {
+        TimerText.text = "00:00";
+        //TimerText.text = Time.ToString();
+    }
+}
