26219cc9af93d73d5b77313b87b1bec6b164ba6c
Jin <bomjan@naver.com>
2020-04-20 00:00:32 -0400

    Revert "Merge remote-tracking branch 'origin/UI-Fix' into merging"
    This reverts commit 3c70725cda1fa133483989a6303816e3db7ed8a1, reversing
    changes made to ab93896e024406e0cf6122ccaeae6a2c79584b8a.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,6 @@
 //It will communicate with server and affect the game flow(player turn)
 public class GameManager : MonoBehaviour
 {
-	public GameObject UIManager;
-	public UIManager ui;
 	public static GameManager instance { get; private set; }
 	//will be used for other players
 	public Player player { get; private set; }//own player
@@ -15,7 +13,6 @@ public class GameManager : MonoBehaviour
 			instance = this;
 		else
 			Destroy(this);
-		ui = UIManager.GetComponent<UIManager>();
 	}
 
 	public void CreatePlayer()
@@ -44,6 +41,5 @@ public class GameManager : MonoBehaviour
 	public void Turn(bool turn)// Will be called at Client OnData Result case / depend on result(valid click or not)
 	{
 		player.Turn(turn);
-		ui.ChangeStatus(turn);
 	}
 }
