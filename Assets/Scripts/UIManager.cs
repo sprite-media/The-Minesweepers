ff00c4f42e57b6efd270a8bfb22b80f333d745fd
Ding Zhang <36141388+Dgit35265@users.noreply.github.com>
2020-04-19 23:56:39 -0400

    Fix Bug

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     public Sprite ThinkingImg;
     public Sprite SleepingImg;
-    public GameObject Player1;
-    public GameObject Player2;
-    public Text P1Text;
-    public Text P2Text;
+    public GameObject Player1Object;
+    public GameObject Player2Object;
+    public GameObject P1TextObject;
+    public GameObject P2TextObject;
 
     private Image P1Img;
     private Image P2Img;
 
     private void Awake()
     {
-        P1Img = Player1.GetComponent<Image>();
-        P2Img = Player2.GetComponent<Image>();
+        P1Img = Player1Object.GetComponent<Image>();
+        P2Img = Player1Object.GetComponent<Image>();
     }
 
     public void ChangeStatus(bool status)
@@ -26,16 +27,16 @@ public class UIManager : MonoBehaviour
         if (status)
         {
             P1Img.sprite = ThinkingImg;
-            P1Text.text = "Thinking...";
+            P1TextObject.GetComponent<TextMeshProUGUI>().text = "Thinking...";
             P2Img.sprite = SleepingImg;
-            P2Text.text = "Waiting...";
+            P2TextObject.GetComponent<TextMeshProUGUI>().text = "Waiting...";
         }
         else
         {
             P1Img.sprite = SleepingImg;
-            P1Text.text = "Waiting...";
+            P1TextObject.GetComponent<TextMeshProUGUI>().text = "Waiting...";
             P2Img.sprite = ThinkingImg;
-            P2Text.text = "Thinking...";
+            P2TextObject.GetComponent<TextMeshProUGUI>().text = "Thinking...";
         }
     }
 }
